674e4f719bcae764566350845c0c654d3de982bb
AqumpusAXY <whitefroth@163.com>
2026-06-27 13:11:23 +0800

    [feat]: 增加可配置的两次更新搜索的延迟

--- src/main/java/github/aqumpusaxy/betterstoragelectern/BSLConfig.java
+++ src/main/java/github/aqumpusaxy/betterstoragelectern/BSLConfig.java
@@ -9,9 +9,11 @@ public final class BSLConfig {
 
     public final ForgeConfigSpec.BooleanValue AUTO_FOCUS_SEARCH_FIELD_ON_OPENED;
     public final ForgeConfigSpec.BooleanValue SAVE_SEARCH_FIELD_CONTENT;
+    public final ForgeConfigSpec.IntValue SEARCH_UPDATE_DELAY;
 
     private static final String PATH_AUTO_FOCUS_SEARCH_FIELD_ON_OPENED = "auto_focus_search_field_on_opened";
     private static final String PATH_SAVE_SEARCH_FIELD_CONTENT = "save_search_field_content";
+    private static final String PATH_SEARCH_UPDATE_DELAY = "search_update_delay";
 
     private BSLConfig(ForgeConfigSpec.Builder builder) {
         AUTO_FOCUS_SEARCH_FIELD_ON_OPENED = builder
@@ -20,6 +22,9 @@ public final class BSLConfig {
         SAVE_SEARCH_FIELD_CONTENT = builder
                 .comment("If the search field content should be saved")
                 .define(PATH_SAVE_SEARCH_FIELD_CONTENT, false);
+        SEARCH_UPDATE_DELAY = builder
+                .comment("The Delay between two search updates")
+                .defineInRange(PATH_SEARCH_UPDATE_DELAY, 200, 0, 1000);
     }
 
     static {
--- src/main/java/github/aqumpusaxy/betterstoragelectern/mixin/MixinAbstractStorageTerminalScreen.java
+++ src/main/java/github/aqumpusaxy/betterstoragelectern/mixin/MixinAbstractStorageTerminalScreen.java
@@ -10,12 +10,17 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(AbstractStorageTerminalScreen.class)
+@Mixin(value = AbstractStorageTerminalScreen.class, remap = false)
 public abstract class MixinAbstractStorageTerminalScreen<T extends StorageTerminalMenu> extends AbstractContainerScreen<T> {
+    @Unique
+    private long BSL$lastSearchTime = 0L;
     @Shadow
     public abstract SortSettings getSortSettings();
 
@@ -57,5 +62,18 @@ public abstract class MixinAbstractStorageTerminalScreen<T extends StorageTermin
     private int modifyMouseScrolledI(int i) {
         //你自己算算就知道是啥了
         return i + (this.getSortSettings() == null || this.getSortSettings().expanded ? -2 : 2);
+    }
+
+    @Inject(
+            method = "containerTick",
+            at = @At("HEAD"),
+            cancellable = true)
+    private void onContainerTick(CallbackInfo ci) {
+        final long CURRENT_TIME = System.currentTimeMillis();
+        if (!(CURRENT_TIME - BSL$lastSearchTime > BSLConfig.INSTANCE.SEARCH_UPDATE_DELAY.get())) {
+            ci.cancel();
+            return;
+        }
+        BSL$lastSearchTime = CURRENT_TIME;
     }
 }
